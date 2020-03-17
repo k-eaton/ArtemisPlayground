a1aae0349d74549a02fb6722115bf3ef1128a306
Irene <iserrano@mills.edu>
2020-03-16 17:57:23 -0700

    initial setup, little mess up in settings,py

--- ArtemisPlayground/settings.py
+++ ArtemisPlayground/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'share.apps.ShareConfig' #iserrano0
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
